c379970ba7e45edb2839f2a6ad63a3cf59243698
ten14301 <ten077710@gmail.com>
2023-04-26 16:04:34 +0700

    finish?

--- index.php
+++ index.php
@@ -1,9 +1,34 @@
 <?php
 session_start();
+require("connection.php");
 if($_SESSION["user_id"]==""){
+  $user_id = $_SESSION['user_id'];
   header("Location: login.php");
 }
+$user_id = $_SESSION['user_id'];
+$sql="SELECT * FROM sets WHERE `user_id` = '$user_id'";
+$result = $conn->query($sql);
+if (!$result) {
+  die("Query failed: " . $conn->error);
+}
+
+$numRows = $result->num_rows;
+$data = [];
+$i=0;
+
+while ($row = $result->fetch_assoc() or $numRows==0) {
+  
+  $data[] = $row["s_name"]; 
+  $numRows++;
+  $i++;
+ 
+  
+}
+$uniqueArray = array_values(array_unique($data));
+
+
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -47,9 +72,50 @@ if($_SESSION["user_id"]==""){
       <div class="container">
         <div class="home">
           <div class="home__content">
-            <h2 class="home__title">Recommendation</h2>
+          <h2 class="home__title">My Collections</h2>
+            
             <div class="home__sets">
-              <div class="home__set">
+              
+                <?php 
+                $x=0;
+                foreach ($uniqueArray as $value) {
+                  ?>
+                  <div class="home__set">
+                  <h3 class="home__set-name"><?php echo $value; ?></h3>
+                  <div class="home__set-images">
+                  <?php
+                  $sql2 = "SELECT * from sets where user_id = '$user_id' and s_name = '$value' ";
+                  $result2 = $conn->query($sql2);
+                  if (!$result2) {
+                    die("Query failed: " . $conn->error);
+                }
+                  
+                  $numRows2 = $result2->num_rows;
+                  while ($row2 = $result2->fetch_assoc()) {
+                    $c_id=$row2["c_id"];
+                    $sql3="SELECT * from clothes where c_id = '$c_id'";
+                    $result3 = mysqli_query($conn, $sql3);
+                    $row3 = mysqli_fetch_assoc($result3);
+                    ?>
+                    
+                    <div class="home__set-image">
+                    <img src="upload_img/<?php echo $row3["image"];?>" alt="" />
+
+                    </div>
+                    <?php
+                    $numRows++;
+                    $x++;
+                }
+                ?>
+                
+                
+                </div>
+                </div>
+                <br>
+                <?php
+              }
+                ?>
+                <!--
                 <h3 class="home__set-name">Set 1</h3>
                 <div class="home__set-images">
                   <div class="home__set-image">
@@ -66,9 +132,11 @@ if($_SESSION["user_id"]==""){
                   </div>
                 </div>
               </div>
+
               <div class="home__set">
-                <h3 class="home__set-name">Set 1</h3>
+                  <h3 class="home__set-name">Set 1</h3>
                 <div class="home__set-images">
+
                   <div class="home__set-image">
                     <img src="/photo-1600269452121-4f2416e55c28.webp" alt="" />
                   </div>
@@ -81,8 +149,10 @@ if($_SESSION["user_id"]==""){
                   <div class="home__set-image">
                     <img src="/photo-1600269452121-4f2416e55c28.webp" alt="" />
                   </div>
+
                 </div>
               </div>
+
               <div class="home__set">
                 <h3 class="home__set-name">Set 1</h3>
                 <div class="home__set-images">
@@ -99,12 +169,15 @@ if($_SESSION["user_id"]==""){
                     <img src="/photo-1600269452121-4f2416e55c28.webp" alt="" />
                   </div>
                 </div>
+              
+              -->
               </div>
             </div>
           </div>
+          <div class="container">
           <div class="home__content">
             <div class="home__title-button">
-              <h2 class="home__title">My Collections</h2>
+            <h2 class="home__title">Recommendation</h2>
               <span class="home__title-create button">
                 <a href="/closet/clothes.php">create new</a>
 
@@ -116,17 +189,27 @@ if($_SESSION["user_id"]==""){
               <div class="home__set">
                 <h3 class="home__set-name">Set 1</h3>
                 <div class="home__set-images">
-                  <div class="home__set-image">
-                    <img src="/photo-1600269452121-4f2416e55c28.webp" alt="" />
-                  </div>
-                  <div class="home__set-image">
-                    <img src="/photo-1600269452121-4f2416e55c28.webp" alt="" />
-                  </div>
-                  <div class="home__set-image">
-                    <img src="/photo-1600269452121-4f2416e55c28.webp" alt="" />
-                  </div>
-                  <div class="home__set-image">
-                    <img src="/photo-1600269452121-4f2416e55c28.webp" alt="" />
+                  <?php 
+                  $sql4 = "select * from clothes where user_id = '$user_id'";
+                  $result4 = $conn->query($sql4);
+                  if (!$result4) {
+                    die("Query failed: " . $conn->error);
+                }
+                  
+                  $numRows4 = $result4->num_rows;
+                  
+                  while ($row4 = $result4->fetch_assoc()) {
+                    $randomNumber = random_int(1, 2);
+                    if($randomNumber==2){
+                      ?>
+                       <div class="home__set-image">
+                       <img src="upload_img/<?php echo $row4["image"];?>" alt="" />
+                      </div>
+                      <?php
+                      }
+                  }
+                  ?>
+                 
                   </div>
                 </div>
               </div>
